e13b4e332ef4d73be35f9112f179a61092a94284
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-11-16 04:10:51 +0200

    Feat Make the search form with QueryScope

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -13,14 +14,33 @@ class Post extends Model
 
     protected $with=['category', 'author'];
 
-    public function category()
+    public function category(): BelongsTo
     {
         // post belongs to one category
         return $this->belongsTo(Category::class);
     }
 
-    public function author()
+    /**
+     * @return BelongsTo  user of a post
+     */
+    public function author(): BelongsTo
     {
         return $this->belongsTo(User::class,'user_id');
+    }
+
+
+    /**
+     * @param $query
+     * @param array $filters
+     *
+     * this scope filter will take a list of filter to do on posts table , give back every match up
+     * that is like to the filter, here we filter through the title or the body
+     */
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false,  fn($query, $search)=>$query
+            ->where('title', 'like', '%'. request('search') .'%')
+            ->orWhere('body', 'like', '%' . request('search'). '%'));
+
     }
 }
